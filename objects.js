1f9a02ca8fa2ef6a6a094d9475dd6e411702f8ad
Nikki M <nrmonegain@gmail.com>
2018-06-22 19:34:39 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {artistName: "songName"}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  Object.assign({playlist}, {artistName:"songTitle"})
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName
+}
