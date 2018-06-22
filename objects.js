bab0098228c91b3a0925c9cc4ca054c408090977
Nikki M <nrmonegain@gmail.com>
2018-06-22 19:36:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = {artistName: "songName"}
 
 function updatePlaylist (playlist, artistName, songTitle) {
   Object.assign({playlist}, {artistName:"songTitle"})
+  return playlist
 }
 
 function removeFromPlaylist (playlist, artistName) {
